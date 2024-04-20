9fe536263461561014de707014c869a6810fc5df
Christina Castillo <chrisnicollo@outlook.com>
2024-04-20 12:47:17 -0400

    clean up code and readme

--- InMemoryDB.py
+++ InMemoryDB.py
@@ -4,11 +4,15 @@ class InMemoryDB:
         self.temp = dict() # The temporary dictionary to be used during transactions
         self.in_progress = False
       
+    # This will always clear the previous existing transaction data
+    # If this is called while a transaction is already in progress, the transaction will be aborted, an exception will be raised, and no new transaction will start
     def begin_transaction(self):
-        # I'm not sure what should happen if this is called when another transaction is already in progress, so I will assume that I can clear the existing transaction data
-        # FIXME: Contact someone about this
         self.temp.clear()
-        self.in_progress = True
+        if self.in_progress:
+            self.in_progress = False
+            raise Exception("can only start a new transaction when there is not already an in-progress transaction")
+        else:
+            self.in_progress = True
     
     def put(self, key: str, value: int):
         if not self.in_progress: raise Exception("can only update keys during an in-progress transaction")
